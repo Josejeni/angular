9a53f86c7a42f5a0cd44c79ef9f259da707ddbf5
josephin <josephinjenifer373@gmail.com>
2022-09-26 19:00:01 +0530

    deploy'

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express=require("express");
 const path=require("path");
 const app=express();
-app.use(express.static(__dirname+'/dist/pro1'));
+app.use(express.static(__dirname+'/dist/edustar-frontend'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist'+'/pro1/index.html'));
+    res.sendFile(path.join(__dirname+'/dist'+'/edustar-frontend/index.html'));
 
 });
 
