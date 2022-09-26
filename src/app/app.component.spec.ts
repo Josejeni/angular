5c6d47214282de98f1af2250e568d9bc1db15d09
josephin <josephinjenifer373@gmail.com>
2022-09-26 19:11:21 +0530

    commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pro1'`, () => {
+  it(`should have as title 'Edustar-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pro1');
+    expect(app.title).toEqual('Edustar-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('pro1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Edustar-frontend app is running!');
   });
 });
